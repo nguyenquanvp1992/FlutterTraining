bd4dbe7341316f1d162749c538a8f6be629965d5
Nguyen Quan <nguyenquan.vp1992@gmail.com>
2020-11-06 11:26:28 +0700

    done UI login

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: <Widget>[
           Stack(
@@ -64,53 +65,54 @@ class _MyHomePageState extends State<MyHomePage> {
             margin: EdgeInsets.only(top: 38.0, left: 20.0, right: 20.0),
             alignment: Alignment.center,
             decoration: BoxDecoration(
-                color: Color(0xff1E562A), borderRadius: BorderRadius.circular(20.0)),
+                color: Color(0xff1E562A),
+                borderRadius: BorderRadius.circular(20.0)),
             child: Text(
               "Facebook",
               style: TextStyle(color: Colors.white, fontSize: 19.0),
             ),
           ),
           Container(
-            height: 44.0,
-            width: 320.0,
-            margin: EdgeInsets.only(top: 5.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(border: Border.all(color: Color(0xff1E562A)), borderRadius: BorderRadius.circular(20.0)),
-            child: Text(
-              "Google",
-              style: TextStyle(color: Color(0xff1E562A), fontSize: 19.0),
-            )
+              height: 44.0,
+              width: 320.0,
+              margin: EdgeInsets.only(top: 5.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xff1E562A)),
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: Text(
+                "Google",
+                style: TextStyle(
+                    color: Color(0xff1E562A),
+                    fontSize: 19.0,
+                    backgroundColor: Colors.white),
+              )),
+          Expanded(
+            child: SizedBox(),
           ),
           Container(
-            alignment: FractionalOffset.bottomCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Điều khoản",
-                      style: TextStyle(color: Color(0xffAB3611), fontSize: 10.0),
-                    ),
-                    Text(
-                      " người dùng và ",
-                      style: TextStyle(color: Color(0xff0F0F0F), fontSize: 10.0),
-                    ),
-                    Text(
-                      "bảo mật",
-                      style: TextStyle(color: Color(0xffAB3611), fontSize: 10.0),
-                    ),
-                    Text(
-                      " thông tin",
-                      style: TextStyle(color: Color(0xff0F0F0F), fontSize: 10.0),
-                    )
-                  ],
-                ),
-              ],
-            )
-          )
+              alignment: FractionalOffset.bottomCenter,
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: RichText(
+                text: TextSpan(children: <TextSpan>[
+                  TextSpan(
+                      text: "Điều khoản",
+                      style:
+                          TextStyle(color: Color(0xffAB3611), fontSize: 10.0)),
+                  TextSpan(
+                      text: " người dùng và",
+                      style:
+                          TextStyle(color: Color(0xff0F0F0F), fontSize: 10.0)),
+                  TextSpan(
+                      text: " bảo mật",
+                      style:
+                          TextStyle(color: Color(0xffAB3611), fontSize: 10.0)),
+                  TextSpan(
+                      text: " thông tin",
+                      style:
+                          TextStyle(color: Color(0xff0F0F0F), fontSize: 10.0))
+                ]),
+              ))
         ],
       ),
     );
